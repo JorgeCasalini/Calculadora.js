540a117f82f81c399d75d1a29b7352ae881de9aa
jorgeCasalini <joshcasalini@gmail.com>
2024-07-22 15:21:20 -0300

    implements theme change and possibility to copy

--- script.js
+++ script.js
@@ -34,7 +34,43 @@ input.addEventListener("keydown", function(ev){
 document.getElementById("equal").addEventListener("click", calculate)
 
 function calculate() {
-   const result = eval(input.value) 
-   resultInput.value = result
+    resultInput.value = 'ERROR'
+    resultInput.classList.add('error')
+
+    const result = eval(input.value) 
+    
+    resultInput.value = result
+    resultInput.classList.remove('error')
 }
+
+document.getElementById('copyToClipboard').addEventListener('click', function(ev){
+    const button = ev.currentTarget
+    if(button.innerText === 'Copy'){
+        button.innerText = 'Copied!'
+        button.classList.add('success')
+        window.navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+document.getElementById("themeSwitcher").addEventListener('click', function(){
+    if(main.dataset.theme === 'dark'){
+        root.style.setProperty('--bg-color', '#F1EED9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#595A5D')
+        root.style.setProperty('--primary-color', '#CF631B')
+        root.style.setProperty('equal-color', '#B06A00')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#595A5D')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#EB7300')
+        root.style.setProperty('equal-color', '#EB8E00')
+        main.dataset.theme = 'dark'
+    }
+
+})
 
